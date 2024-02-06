9cac6322723e8846079855b3404ee13fae776c1d
Karolina Kozubik <k4aro0@gmail.com>
2024-02-06 21:26:14 +0100

    improve validation

--- tools/video_to_frames.py
+++ tools/video_to_frames.py
@@ -1,11 +1,12 @@
 import cv2
 import argparse
+import os
 
 
 def cut_to_frames(input_file: str, output_dir: str) -> None:
-    if not input_file:
+    if not os.path.isfile(input_file):
         raise ValueError(f"{input_file=} is not a valid file path.")
-    if not output_dir:
+    if not os.path.isdir(output_dir):
         raise ValueError(f"{output_dir=} is not a valid directory path.")
     vidcap = cv2.VideoCapture(input_file)
     success, img = vidcap.read()
