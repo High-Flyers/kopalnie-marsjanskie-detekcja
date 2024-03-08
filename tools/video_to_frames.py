a4e6419d561a02e0d56531306a64ed05d31ebd32
Karolina Kozubik <k4aro0@gmail.com>
2024-03-08 23:40:19 +0100

    add cvat_yolo_formatter

--- tools/video_to_frames.py
+++ tools/video_to_frames.py
@@ -3,18 +3,20 @@ import argparse
 import os
 
 
-def cut_to_frames(input_file: str, output_dir: str, count_from: int = 0) -> None:
+def cut_to_frames(input_file: str, output_dir: str, count_from: int = 0) -> int:
     if not os.path.isfile(input_file):
         raise ValueError(f"{input_file=} is not a valid file path.")
     if not os.path.isdir(output_dir):
         raise ValueError(f"{output_dir=} is not a valid directory path.")
+    
     vidcap = cv2.VideoCapture(input_file)
     success, img = vidcap.read()
     count = count_from
     while success:
-        cv2.imwrite(f"{output_dir}/frame{count:04}.jpg", img)
-        success, img = vidcap.read()
+        cv2.imwrite(f"{output_dir}/frame_{count:06}.jpg", img)
         count += 1
+        success, img = vidcap.read()
+    return count - count_from
 
 
 if __name__ == "__main__":
