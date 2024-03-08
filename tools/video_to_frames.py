2153857ebbf40e5895a67f46332437deb4e32407
Karolina Kozubik <k4aro0@gmail.com>
2024-03-08 18:53:51 +0100

    update video_to_frames

--- tools/video_to_frames.py
+++ tools/video_to_frames.py
@@ -3,14 +3,14 @@ import argparse
 import os
 
 
-def cut_to_frames(input_file: str, output_dir: str) -> None:
+def cut_to_frames(input_file: str, output_dir: str, count_from: int = 0) -> None:
     if not os.path.isfile(input_file):
         raise ValueError(f"{input_file=} is not a valid file path.")
     if not os.path.isdir(output_dir):
         raise ValueError(f"{output_dir=} is not a valid directory path.")
     vidcap = cv2.VideoCapture(input_file)
     success, img = vidcap.read()
-    count = 0
+    count = count_from
     while success:
         cv2.imwrite(f"{output_dir}/frame{count:04}.jpg", img)
         success, img = vidcap.read()
